ed6276cf93e968b43f21d3ad78d5e75fef6c22d8
bernice <bernice@hubtel.com>
2022-12-12 12:15:32 +0000

    sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 function Header() {
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
   return (
     <Container>
       <a>
@@ -25,8 +26,68 @@ function Header() {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Account</a>
-        <a href="#">Menu</a>
+        <a href="#" onClick={() => setIsMenuClicked((prev) => !prev)}>
+          Menu
+        </a>
       </RightMenu>
+      {isMenuClicked && (
+        <BurgerNav>
+          <CloseWrap>
+            <CustomClose
+              src="/images/close.svg"
+              onClick={() => setIsMenuClicked((prev) => !prev)}
+            />
+          </CloseWrap>
+          <li>
+            <a href="#">Model S</a>
+          </li>
+          <li>
+            <a href="#">Model 3</a>
+          </li>
+          <li>
+            <a href="#">Model X</a>
+          </li>
+          <li>
+            <a href="#">Model Y</a>
+          </li>
+          <li>
+            <a href="#">Solar Roof</a>
+          </li>
+          <li>
+            <a href="#">Solar Panels</a>
+          </li>
+          <li>
+            <a href="#">Existing Inventory</a>
+          </li>
+          <li>
+            <a href="#">Used Inventory</a>
+          </li>
+          <li>
+            <a href="#">Trade-In</a>
+          </li>
+          <li>
+            <a href="#">Test Drive</a>
+          </li>
+          <li>
+            <a href="#">Insurance</a>
+          </li>
+          <li>
+            <a href="#">Powerwall</a>
+          </li>
+          <li>
+            <a href="#">Utilities</a>
+          </li>
+          <li>
+            <a href="#">Charging</a>
+          </li>
+          <li>
+            <a href="#">Find Us</a>
+          </li>
+          <li>
+            <a href="#">Support</a>
+          </li>
+        </BurgerNav>
+      )}
     </Container>
   );
 }
@@ -43,6 +104,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
   color: rgb(107, 114, 123);
 `;
 const Menu = styled.div`
@@ -78,4 +140,41 @@ const RightMenu = styled.div`
       display: none;
     }
   }
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 10;
+  list-style: none;
+  padding: 20px;
+  max-height: 100vh;
+  overflow: scroll;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+
+  li {
+    padding: 15px 0;
+
+    a {
+      font-weight: 500;
+    }
+  }
+`;
+
+const CustomClose = styled.img`
+  width: 20px;
+  height: 20px;
+  object-fit: contain;
+  cursor: pointer;
+`;
+
+const CloseWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
